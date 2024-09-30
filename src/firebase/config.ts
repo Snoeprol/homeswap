38229302381e78efa971e5d9dbaa474bbe09b23d
mario <mariovanrooij@hotmail.com>
2024-09-30 22:20:52 +0200

    chats kinda semi works

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,7 +5,6 @@ import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: "https://homeswap-c10b7-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
